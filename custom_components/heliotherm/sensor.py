30a721549f6c9aadd9b2f2a8fefcf967c1cdaadd
tsoiks <einfachpost@me.com>
2025-12-29 22:01:41 +0100

    UnitOf* updates

--- custom_components/heliotherm/sensor.py
+++ custom_components/heliotherm/sensor.py
@@ -22,7 +22,7 @@ from homeassistant.components.sensor import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     STATE_UNAVAILABLE,
-    TEMP_CELSIUS,
+    UnitOfTemperature,
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
